522b07ad14c995db3e0da87ee198633d26444374
priyankainfabio <priyanka@infabio.com>
2026-06-05 11:30:36 +0530

    Fix clean routes for Vercel deployment

--- cleanup-static.js
+++ cleanup-static.js
@@ -21,6 +21,11 @@ for (const file of files) {
   html = html.replace(/<noscript><iframe[^>]*googletagmanager[\s\S]*?<\/iframe><\/noscript>/gi, "");
   html = html.replace(/https?:\/\/diabetes\.wellpro\.one\/?(?:index)?/gi, "/diabetes-reversal/index/");
   html = html.replace(/https?:\/\/pcod\.wellpro\.one\/?(?:index)?/gi, "/pcod/index/");
+  html = html.replace(/\/diabetes-reversal\/index\/?/gi, "/diabetes-reversal/");
+  html = html.replace(/\/doctor-assist-registration\/index\/?/gi, "/doctor-assist-registration/");
+  html = html.replace(/\/cholesterol\/index\/?/gi, "/cholesterol/");
+  html = html.replace(/\/pcod\/index\/?/gi, "/pcod/");
+  html = html.replace(/\/soleus\/index\/?/gi, "/soleus/");
   html = html.replace(/\s+target="_blank"/g, "");
   fs.writeFileSync(file, html);
 }
